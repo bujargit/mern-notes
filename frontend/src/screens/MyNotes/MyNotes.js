654b4c9c95954d908927de211943098ce7f5ee77
unknown <buj4r.hyseni@gmail.com>
2023-10-06 12:34:21 +0200

    Note Redux

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,37 +1,49 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect } from "react";
+// import axios from "axios";
 import MainScreen from "../../components/MainScreen";
 import { Link } from "react-router-dom";
 import { Accordion, Badge, Button } from "react-bootstrap";
-// import notes from "../../data/notes";
+import { useDispatch, useSelector } from "react-redux";
+import { listNotes } from "../../actions/notesActions";
+import Loading from "../../components/Loading";
+import ErrorMessage from "../../components/ErrorMessage";
+import { useNavigate } from "react-router-dom";
 
 const MyNotes = () => {
-  const [notes, setNotes] = useState([]);
+  const dispatch = useDispatch();
+
+  const noteList = useSelector((state) => state.noteList);
+  const { loading, notes, error } = noteList;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const navigate = useNavigate();
 
   const deleteHandler = (id) => {
     if (window.confirm("Are you Sure?")) {
     }
   };
 
-  const fetchNotes = async () => {
-    const { data } = await axios.get("/api/notes");
-    setNotes(data);
-  };
-
   console.log(notes);
 
   useEffect(() => {
-    fetchNotes();
-  }, []);
+    dispatch(listNotes());
+    if (!userInfo) {
+      navigate("/");
+    }
+  }, [dispatch]);
 
   return (
-    <MainScreen title="Welcome back Bujar Hyseni...">
+    <MainScreen title={`Welcome Back ${userInfo.name}...`}>
       <Link to="/createnote">
         <Button style={{ marginLeft: 10, marginBottom: 6 }}>
           Create New Note
         </Button>
       </Link>
-      {notes.map((note) => (
+      {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+      {loading && <Loading />}
+      {notes?.map((note) => (
         <Accordion className="card" style={{ margin: 10 }} key={note._id}>
           <Accordion.Item eventKey="0">
             <div className="edit-delete-holder">
@@ -56,7 +68,12 @@ const MyNotes = () => {
               </h4>
               <blockquote className="blockquote mb-0">
                 <p>{note.content}</p>
-                <footer className="blockquote-footer">Created On - date</footer>
+                <footer className="blockquote-footer">
+                  Created on{" "}
+                  <cite title="Source Title">
+                    {note.createdAt.substring(0, 10)}
+                  </cite>
+                </footer>
               </blockquote>
             </Accordion.Body>
           </Accordion.Item>
