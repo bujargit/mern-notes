81a8b6ce5b5fe0fa64843fb5ba7e631a0823eed2
unknown <buj4r.hyseni@gmail.com>
2023-10-12 09:51:40 +0200

    Deployment ready

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const noteRoutes = require("./routes/noteRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path");
 
 dotenv.config();
 
@@ -29,6 +30,23 @@ app.use(express.json());
 
 app.use("/api/notes", noteRoutes);
 app.use("/api/users", userRoutes);
+
+// ------------ Deployment ------------
+
+const __dirname1 = path.resolve();
+if ((process.env.NODE_ENV === "production")) {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
+// ------------ deployment ------------
 
 app.use(notFound);
 app.use(errorHandler);
